617624859b1a20b618082f511ee314a0a9b39970
SmitomB <smitomb@gmail.com>
2021-06-27 13:01:40 -0400

    Script to practice Chapter 2 codes

---
+++ Ch2NormalModeling.R
@@ -0,0 +1,7 @@
+# Title: Normal Modeling - A chapter from Bayesian Essential with R
+# Author: Smitom Borah
+# Date: 06/27/2021
+
+# Background ----
+## This script contains some of the codes that were required to learn the concepts
+## of Bayesian modeling. 
